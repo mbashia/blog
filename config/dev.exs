ac0b5409a3df821b47c591e5cbac3fb153d57ffc
mbashia <mbashiavictor@gmail.com>
2024-09-17 16:49:55 +0300

    added

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ import Config
 # Configure your database
 config :blog, Blog.Repo,
   username: "root",
-  password: "",
+  password: System.get_env("MYSQL_PASS"),
   hostname: "localhost",
   database: "blog_dev",
   stacktrace: true,
